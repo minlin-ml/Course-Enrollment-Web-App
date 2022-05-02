7a94cf258388ae630362b6182f94e812b939d90a
minlin-nilnim <min-lin@live.cn>
2022-05-02 14:13:54 -0400

    create enrollment page and update enrollment template -closes #25

--- application/routes.py
+++ application/routes.py
@@ -4,9 +4,6 @@ from flask import Response, redirect, render_template, request, json, flash, red
 from application.models import User, Course, Enrollment
 from application.forms import LoginForm, RegisterForm
 
-courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
-
-
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
@@ -62,10 +59,19 @@ def register():
 
 @app.route("/enrollment", methods=["GET", "POST"])
 def enrollment():
-  id = request.form.get('courseID')
-  title = request.form.get('title')
+  courseID = request.form.get('courseID')
+  courseTitle = request.form.get('title')
+  user_id = 1
+  if courseID:
+    if Enrollment.objects(user_id=user_id, courseID=courseID):
+      flash(f"Opps! You are already registered in this course {courseTitle}!","danger" )
+      return redirect(url_for('courses'))
+    else:
+      Enrollment(user_id=user_id,courseID=courseID)
+      flash(f"You registered to {courseTitle} successfully!", "success")
+  classes = None
   term = request.form.get('term')
-  return render_template("enrollment.html", enrollment=True, data={"id": id, "title": title, "term": term})
+  return render_template("enrollment.html", enrollment=True, title = "Enrollment", classes=classes)
 
 @app.route("/api/")
 @app.route("/api/<idx>")
