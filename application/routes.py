e67a30ed785c0b115856b5ccec4bed9357cfa8e8
minlin-nilnim <min-lin@live.cn>
2022-05-02 14:53:01 -0400

    create the aggregation pipeline to process data -closes #26

--- application/routes.py
+++ application/routes.py
@@ -67,9 +67,44 @@ def enrollment():
       flash(f"Opps! You are already registered in this course {courseTitle}!","danger" )
       return redirect(url_for('courses'))
     else:
-      Enrollment(user_id=user_id,courseID=courseID)
+      Enrollment(user_id=user_id,courseID=courseID).save()
       flash(f"You registered to {courseTitle} successfully!", "success")
-  classes = None
+  classes = list(User.objects.aggregate(*[
+          {
+              '$lookup': {
+                  'from': 'enrollment', 
+                  'localField': 'user_id', 
+                  'foreignField': 'user_id', 
+                  'as': 'r1'
+              }
+          }, {
+              '$unwind': {
+                  'path': '$r1', 
+                  'includeArrayIndex': 'r1.id', 
+                  'preserveNullAndEmptyArrays': False
+              }
+          }, {
+              '$lookup': {
+                  'from': 'course', 
+                  'localField': 'r1.courseID', 
+                  'foreignField': 'courseID', 
+                  'as': 'r2'
+              }
+          }, {
+              '$unwind': {
+                  'path': '$r2', 
+                  'preserveNullAndEmptyArrays': False
+              }
+          }, {
+              '$match': {
+                  'user_id': user_id
+              }
+          }, {
+              '$sort': {
+                  'courseID': 1
+              }
+          }
+      ]))
   term = request.form.get('term')
   return render_template("enrollment.html", enrollment=True, title = "Enrollment", classes=classes)
 
